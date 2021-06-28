2510d5a36ba0d786109f57fb467a3586f8c7acfa
ultrasoft011 <ultrasoft.011@gmail.com>
2021-06-28 10:48:02 -0500

    handling server errors

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -11,3 +11,19 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const getRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886zzz'
+    );
+    const data = await res.json();
+    if(!res.ok) throw Error (`${data.message} ${res.status}`)
+    
+    console.log(res, data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getRecipe();
